a4760ccdf4fcf2639ea84cba5a39e475f419b6ea
Anubhav Saxena <anubhav.s@tringapps.com>
2020-08-17 05:51:44 -0500

    issue one is fixed L1 and L2 labels should not come from starting. Follow the instruction given in storyboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,8 +19,114 @@ var app = angular.module('playerApp', ['ngSanitize']);
         thisRef.oneWayArrowDresc = false;
         thisRef.bothWayArrowDresc = false;
         thisRef.fourparticle = true;
+        thisRef.showLabels = false;
         thisRef.chainOfParticle = false;
-        
+        thisRef.pause = function(){
+            angular.element('.vibrator').removeClass('vibrate');
+            // angular.element('.startsim').html('Start')
+            
+            angular.element('.wave').remove();
+            angular.element('.vibrationArea').children('element').append(`<div class="wave plane_wave" id="wave_1"></div>
+            <div class="wave plane_wave" id="wave_2"></div>
+            <div class="wave plane_wave" id="wave_3"></div>
+            <div class="wave plane_wave" id="wave_4"></div>
+            <div class="wave plane_wave" id="wave_5"></div>
+            <div class="wave plane_wave" id="wave_6"></div>
+            <div class="wave plane_wave" id="wave_7"></div>
+            <div class="wave plane_wave" id="wave_8"></div>
+            <div class="wave plane_wave" id="wave_9"></div>
+            <div class="wave plane_wave" id="wave_10"></div>
+            <div class="wave plane_wave" id="wave_11"></div>
+            <div class="wave plane_wave" id="wave_12"></div>
+            <div class="wave plane_wave" id="wave_13"></div>
+            <div class="wave plane_wave" id="wave_14"></div>
+            <div class="wave plane_wave" id="wave_15"></div>
+            <div class="wave plane_wave" id="wave_16"></div>
+            <div class="wave plane_wave" id="wave_17"></div>
+            <div class="wave plane_wave" id="wave_18"></div>
+            <div class="wave plane_wave" id="wave_19"></div>
+            <div class="wave red_particle_wave" id="wave_20"></div>
+            <div class="wave plane_wave" id="wave_21"></div>
+            <div class="wave plane_wave" id="wave_22"></div>
+            <div class="wave plane_wave" id="wave_23"></div>
+            <div class="wave plane_wave" id="wave_24"></div>
+            <div class="wave plane_wave" id="wave_25"></div>
+            <div class="wave plane_wave" id="wave_26"></div>
+            <div class="wave plane_wave" id="wave_27"></div>
+            <div class="wave plane_wave" id="wave_28"></div>
+            <div class="wave plane_wave" id="wave_29"></div>
+            <div class="wave blue_particle_wave" id="wave_30"></div>
+            <div class="wave plane_wave" id="wave_31"></div>
+            <div class="wave plane_wave" id="wave_32"></div>
+            <div class="wave plane_wave" id="wave_33"></div>
+            <div class="wave plane_wave" id="wave_34"></div>
+            <div class="wave plane_wave" id="wave_35"></div>
+            <div class="wave green_particle_wave" id="wave_36"></div>
+            <div class="wave plane_wave" id="wave_37"></div>
+            <div class="wave plane_wave" id="wave_38"></div>
+            <div class="wave plane_wave" id="wave_39"></div>
+            <div class="wave plane_wave" id="wave_40"></div>
+            <div class="wave plane_wave" id="wave_41"></div>
+            <div class="wave plane_wave" id="wave_42"></div>
+            <div class="wave plane_wave" id="wave_43"></div>
+            <div class="wave plane_wave" id="wave_44"></div>
+            <div class="wave plane_wave" id="wave_45"></div>
+            <div class="wave plane_wave" id="wave_46"></div>
+            <div class="wave plane_wave" id="wave_47"></div>
+            <div class="wave magenta_particle_wave" id="wave_48"></div>
+            <div class="wave plane_wave" id="wave_49"></div>
+            <div class="wave plane_wave" id="wave_50"></div>
+            <div class="wave plane_wave" id="wave_51"></div>
+            <div class="wave plane_wave" id="wave_52"></div>
+            <div class="wave plane_wave" id="wave_53"></div>
+            <div class="wave plane_wave" id="wave_54"></div>
+            <div class="wave plane_wave" id="wave_55"></div>
+            <div class="wave plane_wave" id="wave_56"></div>
+            <div class="wave plane_wave" id="wave_57"></div>
+            <div class="wave plane_wave" id="wave_58"></div>
+            <div class="wave plane_wave" id="wave_59"></div>
+            <div class="wave plane_wave" id="wave_60"></div>`);
+            var left = 90;
+            var waves = angular.element('.wave');
+            waves.each(function(i,v){
+                
+                var strID = v.id;
+                    var id = parseInt(strID.split('_')[1]);
+                    left = left + (30/2.2);
+                    if(thisRef.animMode ==='fourparticles') {
+                        
+                        if(i+1 === id) {
+                            angular.element('#'+strID).css('left',left+'px');
+                            
+                        }
+                        if(id%2 ===0) {
+                            if(angular.element('#'+strID).hasClass('red_particle_wave') && angular.element('#'+strID).hasClass('green_particle_wave') && angular.element('#'+strID).hasClass('magenta_particle_wave') && angular.element('#'+strID).hasClass('blue_particle_wave')) {
+                                angular.element('#'+strID).css('left',left+'px');
+                                angular.element('#'+strID).removeClass('plane_wave').addClass('white_particle_wave')
+                            }
+                            if(angular.element('#'+strID).hasClass('red_particle_wave') || angular.element('#'+strID).hasClass('green_particle_wave') || angular.element('#'+strID).hasClass('magenta_particle_wave') || angular.element('#'+strID).hasClass('blue_particle_wave')) {
+                                angular.element('#'+strID).css('left',left-5+'px');
+                            }
+                        }
+                    } else{
+                        
+                        if(i+1 === id) {
+                            angular.element('#'+strID).css('left',left+'px');
+                            
+                        }
+                        if((id%2 === 0) && (id !== 20) && (id !== 48) && (id !== 36) && (id !== 30) ) {
+                        
+                            angular.element(v).addClass('white_particle_wave').removeClass('plane_wave')
+                            angular.element(v).css('left',left-5+'px');
+                        }
+                        if(angular.element('#'+strID).hasClass('red_particle_wave') || angular.element('#'+strID).hasClass('green_particle_wave') || angular.element('#'+strID).hasClass('magenta_particle_wave') || angular.element('#'+strID).hasClass('blue_particle_wave')) {
+                            angular.element('#'+strID).css('left',left-5+'px');
+                        }
+                    }
+                    
+                
+            });
+        }
         thisRef.startMove = function (e) {
             thisRef.bText = e.target.textContent;
             thisRef.directionOfVibration = "Direction of vibration of the particles";
@@ -31,17 +137,22 @@ var app = angular.module('playerApp', ['ngSanitize']);
                 thisRef.runMotionWaves(e);
                 angular.element(e.target).html("Pause");
                 angular.element('.vibrator').addClass('vibrate');
+                thisRef.showLabels = true;
                 // angular.element('.wave').addClass('wave-oscillation');
 
             }
             if (thisRef.bText == "Pause") {
               angular.element(e.target).html("Continue");
               angular.element('.vibrator').removeClass('vibrate');
+              thisRef.showLabels = false;
+              thisRef.pause();
               
             }
             if (thisRef.bText == "Continue") {
               angular.element(e.target).html("Pause");
               angular.element('.vibrator').addClass('vibrate');
+              thisRef.drawMotionLines(angular.element('.wave'));
+              thisRef.showLabels = true;
             }
           };
 
@@ -149,7 +260,8 @@ var app = angular.module('playerApp', ['ngSanitize']);
             thisRef.oneWayArrowDresc = false
             thisRef.bothWayArrowDresc = false
             thisRef.fourparticle = true
-            thisRef.chainOfParticle = false
+            thisRef.chainOfParticle = false;
+            thisRef.showLabels = false;
             thisRef.animMode = 'fourparticles';
             angular.element('.vibrator').removeClass('vibrate');
             angular.element('.startsim').html('Start')
